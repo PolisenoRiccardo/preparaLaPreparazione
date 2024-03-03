d5a19275098c4634d6191812b1f3f35aa1955808
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2024-03-03 15:42:13 +0000

    punto 8

--- email-poliseno/src/app/sent-mail/sent-mail.component.ts
+++ email-poliseno/src/app/sent-mail/sent-mail.component.ts
@@ -7,14 +7,21 @@ import { Email } from '../app.mail';
   styleUrls: ['./sent-mail.component.css']
 })
 export class SentMailComponent {
-  @HostBinding('attr.class') cssClass = 'card mt-3';
+  @HostBinding('attr.class') cssColor = 'card mt-3';
   @Input() mail: Email;
   nascondi : boolean;
+  mostra : string;
   constructor() {
     this.mail = new Email('Sandro', 'Scusami', 'Dopo spinterogeno...')
     this.nascondi = false
+    this.mostra = 'Espandi'
   }
   espandi() {
-    this.nascondi = !this.nascondi // inverte il boolean 
+    this.nascondi = !this.nascondi;  // inverte il boolean 
+  }
+  speciale() {
+    if (this.cssColor == 'card bg-warning mt-3') {
+      this.cssColor = 'card mt-3';
+    } else this.cssColor = 'card bg-warning mt-3';
   }
 }
